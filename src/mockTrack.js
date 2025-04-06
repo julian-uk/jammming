01100a61f826ec58e079657baf139510f74e1cd1
julian-uk <julianleung.uk@gmail.com>
2025-04-07 07:56:23 +0800

    add missing files

---
+++ src/mockTrack.js
@@ -0,0 +1,26 @@
+// File: mockData.js
+const mockTracks = [
+    {
+      id: '1',
+      name: 'Song A',
+      artist: 'Artist A',
+      album: 'Album A',
+      uri: 'spotify:track:1',
+    },
+    {
+      id: '2',
+      name: 'Song B',
+      artist: 'Artist B',
+      album: 'Album B',
+      uri: 'spotify:track:2',
+    },
+    {
+      id: '3',
+      name: 'Song C',
+      artist: 'Artist C',
+      album: 'Album C',
+      uri: 'spotify:track:3',
+    },
+  ];
+  
+  export default mockTracks;
